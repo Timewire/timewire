293d1fe33877e1fb788f22d6009c65cbd2539b6d
Christina Rebel <christinarebel@Christinas-MacBook-Pro-2.local>
2013-02-13 23:29:31 +0000

    adding css selectors

--- livewire/livewire/urls.py
+++ livewire/livewire/urls.py
@@ -1,23 +1,63 @@
 from livetime.models import Domain
 
-try:
-    Domain.objects.get(url='bbc.co.uk')
-except Domain.DoesNotExist:        
-    bbc = Domain(url='news.bbc.co.uk',
-                 title_selector="h1.story-header",
-                 date_selector="span.date",
-                 date_fmt="%d %B %Y")
-    bbc.save()
+def sites = {'bbc.co.uk':{'title_selector':'h1 .story-header',
+                                'date_selector':'meta[property=OriginalPublicationDate]',
+                                'date_fmt':"%Y/%m/%d %H:%M:%S"},
+            'guardian.co.uk': {'title_selector':'meta title',
+                               'date_selector':'meta[property=article:published_time]&content',
+                               'date_fmt':"%Y-%m-%dT%H:%M:%S%z"},
+            'huffingtonpost.com': {'title_selector':'#main-article-info h1',
+                               'date_selector':'meta meta[property=publish_date]',
+                               'date_fmt':"%a, %d %b %Y %H:%M:%S"}
+            'haaretz.com/news': {'title_selector':'meta[property=og:title]&content',
+                               'date_selector':'.author span.date',
+                               'date_fmt':"%b.%d, %Y | %I:%M %p"},                   
+            'nytimes.com': {'title_selector':'meta[property=og:title]&content',
+                               'date_selector':'meta[property=pdate]',
+                               'date_fmt':"%Y%m%d"}, 
+            'articles.latimes.com': {'title_selector':'meta[property=og:title]&content',
+                               'date_selector':'span.pubdate a&href',
+                               'date_fmt':"%B %d, %Y"}, 
+            'blog.amnestyusa.org': {'title_selector':'article h1.entry-title',
+                               'date_selector':'header div div.date',
+                               'date_fmt':"%B %d, %Y at %I:%M %p"},
+            'globalpost.com': {'title_selector':"title",
+                               'date_selector':'meta[property=article:published_time]&content',
+                               'date_fmt':"%Y-%m-%dT%H:%M:%S%z"},
+            'telegraph.co.uk/news': {'title_selector':'meta[name=title]&content',
+                               'date_selector':'meta[name=last-modified]&content',
+                               'date_fmt':"%Y-%m-%d"},
+            'reuters.com/article': {'title_selector':'meta[name=title]&content',
+                               'date_selector':'meta[name=REVISION_DATE]&content',
+                               'date_fmt':"%A %b %d %H:%M:%S %Z %Y"},
+            'independent.co.uk/news': {'title_selector':'meta[name=title]&content',
+                               'date_selector':'meta[property=article:published_time]&content',
+                               'date_fmt':"%Y-%m-%d"},
 
-try:
-    Domain.objects.get(url='guardian.co.uk')
-except Domain.DoesNotExist:
-    guardian = Domain(url='guardian.co.uk',
-                      title_selector='#main-article-info h1',
-                      date_selector='.publication time',
-                      date_fmt="%A %d %B %Y %H.%M %Z")
-    
-    guardian.save()
+
+
+
+            'common': {'title_selector':["title",'meta[property=og:title]&content'],
+                               'date_selector':['meta[property=pdate]'],
+                               'date_fmt':["%Y%m%d"],                   
+            }
+
+
+for site in sites:
+    create_if_doesnt_exist()
+
+
+def create_if_doesnt_exist(url):
+    try:  
+        Domain.objects.get(url='bbc.co.uk')
+    except Domain.DoesNotExist:
+        site_data = sites[url]
+        site_data['url'] = url
+        site = Domain(**site_data)
+        site.save()
+
+
+
 
 
 
