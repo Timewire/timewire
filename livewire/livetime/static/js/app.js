faaa755a85b492c90605d9b72df3b26dccb1600c
salfield <salfield@root@open.coop>
2013-03-12 21:32:03 +0000

    refactored ui with scroll and zoom implementation

--- livewire/livetime/static/js/app.js
+++ livewire/livetime/static/js/app.js
@@ -22,11 +22,293 @@ var TimeWire = angular.module('TimeWire', ['ui', 'ngCookies', 'filters'])
 	    .otherwise({redirectTo: '/'});
     }]);
 
-//TimeWire.run(function($rootScope, $q, $route) {
-    //set rootscope variables here - be aware the route is not yet initialised
 
-//});
+TimeWire.run(function($rootScope) {
+    //set rootscope variables here - be aware the route and dom are not yet initialise
+});
 
+var pk_incrementor = function () {
+    this.start = 0;
+    this.incr = function () {
+	this.start++;
+	return this.start;
+    };
+};
+
+
+var TimeLine_incr = new pk_incrementor();
+var TimeLine = function (timewire) {
+    this.date_box_width = 35;
+    this.date_marker_width = 2;
+    this.day_marker_width = 1;
+    this.number_of_segments = 3;
+    var timeline = this;
+
+    this.pk = TimeLine_incr.incr();
+    this.width = timewire.offsetWidth;
+    this.dragstart_pos = 0;
+
+
+    this.segments = _.map(_.range(0, this.number_of_segments), function (i) {
+	return new TimeLineSeg(timeline);
+    });
+
+    this.init = function(start_date, end_date) {
+	// called on loading a timewire for this first time
+	this.start_date = start_date;
+	this.end_date = end_date;
+	this.duration = this.end_date - this.start_date;
+
+	var seg_start_date = new Date(this.start_date.getTime() - (this.duration * (Math.floor(this.number_of_segments/2))));
+
+	_.each(this.segments, function (seg) {
+	    seg.init(seg_start_date);
+	    seg_start_date = new Date(seg_start_date.getTime() + timeline.duration);
+	    return seg;
+	});
+    };
+
+    this.add_segment = function(side) {
+	if (side === "left") {
+	    var seg = new TimeLineSeg(timeline);
+	    seg.set(new Date(this.segments[0].start_date.getTime() - this.duration));
+	    this.segments[0] = seg; //insert at beginning of array
+	} else if (side === "right") {
+	    var seg = new TimeLineSeg(timeline);
+	    seg.set(new Date(this.end_date.getTime()));
+	    this.segments[this.segments.length] = seg;
+	}
+    };
+
+
+    this.handle_dragstart = function ($event) {
+	this.dragstart_pos = $event.pageX;
+    };
+    this.handle_drag = function($event) {
+	_.each(this.segments, function (seg) {
+	    seg.handle_move_event($event);
+	});
+	//add new segments where necessary
+    };
+    this.handle_dragend = function($event) {
+	this.handle_drag($event);
+	_.each(this.segments, function (seg) {
+	    seg.pre_drag_left = seg.left;
+	});
+	// change_url
+    };
+
+    this.zoom = function (factor, zoom_point) {
+	if (zoom_point==undefined) {
+	    zoom_point = new Date(this.start_date.getTime() + this.duration/2);
+	}
+	this.duration = this.duration / factor;
+	var half_new_duration = this.duration / 2;
+	this.start_date = new Date(zoom_point.getTime() - half_new_duration);
+	this.end_date = new Date(zoom_point.getTime() + half_new_duration); //half the new duration is on either side of the zoom point
+
+	var seg_start = this.get_date_from_pos(this.segments[0].left);
+	_.each(this.segments, function (seg) {
+	    seg.set_date(seg_start);
+	    seg_start = new Date(seg_start.getTime() + timeline.duration);
+	});
+    };
+
+    this.get_date_pos = function (event_start_date) {
+	return (this.width * (event_start_date - this.start_date)/this.duration).toFixed(0);
+    };
+
+    this.get_date_from_pos = function (pos) {
+	// pos is a position from the timeline start_date which is at 0
+	return new Date(this.start_date.getTime() + this.duration * (pos / this.width));
+
+    };
+    this.how_many_markers = function (level) {
+	var num_markers = 0;
+	if (level === "year") {
+	    num_markers =  _.range(timeline.start_date.getFullYear(), timeline.end_date.getFullYear()).length;
+	} else if (level === "quarter") {
+	    num_markers = timeline.how_many_markers('month')[1]/3;
+	} else if (level === "month") {
+	    var years = timeline.how_many_markers('year')[1];
+	    num_markers = Math.max((years-1), 0) * 12;
+	    if (years>0) {
+		num_markers += _.range(0, timeline.end_date.getMonth()+1).length;
+		num_markers += _.range(timeline.start_date.getMonth(), 12).length;
+	    } else {
+		num_markers += _.range(timeline.start_date.getMonth(), timeline.end_date.getMonth()).length;
+	    }
+	} else if (level === "day") {
+	    var months = timeline.how_many_markers("month")[1];
+	    num_markers = Math.max((months-1), 0) * 30; // approximation of 30 days to month
+	    if (months>0) {
+		num_markers += _.range(0, timeline.end_date.getDate()).length;
+		num_markers += _.range(timeline.start_date.getDate(), 30).length;
+	    } else {
+		num_markers += _.range(timeline.start_date.getDate(), timeline.end_date.getDate()+1).length;
+	    }
+	}
+	return [level, num_markers];
+    };
+
+    this.get_date_markers = function () {
+	var max_markers = this.width/(this.date_box_width*2.5);
+	this.level = "year";
+	var levels = _.map(["year", "quarter", "month", "day"], timeline.how_many_markers);
+	_.each(levels, function(l) {
+	    if (l[1] <= max_markers) {
+		timeline.level = l[0];
+	    }
+	});
+	var markers = {};
+	_.each(this.segments, function(seg) {;
+	    markers[seg.pk] = seg.get_all_markers(1, timeline.level);
+	});
+	return markers;
+    };
+
+};
+
+var TimeLineSeg_incr = new pk_incrementor();
+var TimeLineSeg = function (timewire) {
+    var that = this;
+    this.pk = TimeLineSeg_incr.incr();
+    this.timewire = timewire;
+    this.shown = false;
+
+    this.set_position = function () {
+	this.left = this.timewire.get_date_pos(this.start_date);
+	this.pre_drag_left = this.left;
+	this.check_shown();
+    };
+    this.set_date = function(start_date) {
+	// called on zoom
+	this.start_date = start_date;
+	this.end_date = new Date(start_date.getTime() + timewire.duration);
+    };
+    this.init = function (start_date) {
+	// called only on:
+	// a) timewire initialisation
+	// b) browser resize - i.e. always the width of the timewire.
+	this.set_date(start_date);
+	this.set_position();
+    };
+    this.handle_move_event = function ($event) {
+ 	that.left = Number(that.pre_drag_left) + $event.pageX - that.timewire.dragstart_pos;
+	that.check_shown();
+    };
+    this.check_shown = function () {
+    	if (this.end_date > this.timewire.start_date && this.start_date < this.timewire.end_date) {
+	    this.shown = true;
+	} else {
+	    this.shown = false;
+	}
+    };
+
+
+
+    this.get_marker_position = function(date) {
+	var left = that.timewire.get_date_pos(date);
+	var day_txt = "";
+	var month_txt = "";
+	var year_txt = "";
+	var marker_height = 3;
+	if (that.level === 'day') {
+	    day_txt = date.getDate();
+	}
+	if (that.level === 'month' || (that.level=='day' && !that.has_month_marker())) {
+	    month_txt = months_fmt[date.getMonth()];
+	}
+	if (date.getMonth() === 0 || !that.has_year_marker() && date.getDate()==1 || (!that.has_year_marker() && !that.has_month_marker())) {
+	    year_txt = date.getFullYear();
+	    marker_height = 6;
+	}
+
+	return {'year_txt':year_txt,
+		'month_txt':month_txt,
+		'day_txt':day_txt,
+		'left':left-that.timewire.date_marker_width/2,
+		'box_left':left - that.timewire.date_box_width/2,
+		'height':marker_height};
+    };
+
+    this.get_all_markers = function (interval, level) {
+	var markers = [];
+	if (interval===undefined) {
+	    interval = 1;
+	}
+	var context = {'level':level, 'has_year_marker':this.has_year_marker(), 'has_month_marker':this.has_month_marker()};
+	if (level === 'year') {
+	    markers = _.map(
+			  _.map(_.range(this.start_date.getFullYear()+1, this.end_date.getFullYear()+1), function(year) {
+			      return new Date(year, 0, 1);
+			  }),
+			  this.get_marker_position, context);
+	} else if (level === 'quarter') {
+	    markers = this.get_all_markers(3, "month");
+	} else if (level === 'month') {
+	    // per-month markers previously to the first "year marker"
+	    var year = this.start_date.getFullYear();
+	    markers = _.union(markers, _.map(_.range(this.start_date.getMonth() + 1 + (12-(this.start_date.getMonth()+1)) % interval, 12, interval), function (month) {
+
+		return new Date(year, month, 1);
+		}));
+
+
+	    // per-month markers between the marked years
+	    _.each(_.range(this.start_date.getFullYear()+1, this.end_date.getFullYear()), function(year) {
+		markers = _.union(markers, _.map(_.range(0, 12, interval), function (month) {
+		    return new Date(year, month, 1);
+		}));
+	    });
+	    // per month markers after the last "year marker"
+	    year = this.end_date.getFullYear();
+	    markers = _.union(markers, _.map(_.range(0, this.end_date.getMonth()+1, interval), function (month) {
+		    return new Date(year, month, 1);
+		}));
+	    markers = _.map(markers, this.get_marker_position, context);
+	} else if (level === 'day') {
+	    // per-day markers between the marked months
+	    var date = new Date(this.start_date);
+	    var i = 0;
+	    while (date < this.end_date) {
+		markers[i] = date;
+		date = new Date(date);
+		i += 1;
+		date.setDate(date.getDate() + 1);
+
+		    //date += 1000*60*60*24; // day
+	    }
+	    markers = _.map(markers, this.get_marker_position, context);
+	}
+	return markers;
+    };
+
+    this.has_month_marker = function () {
+	this.start_date.getMonth();
+	if (this.has_year_marker()) {
+	    return true;
+	} else if ((this.end_date.getMonth() - this.start_date.getMonth()) > 0){
+	    return true;
+	}
+	return false;
+    };
+    this.has_year_marker = function () {
+	if ((this.end_date.getFullYear() - this.start_date.getFullYear()) > 0) {
+	    return true;
+	}
+	return false;
+    };
+ };
+
+
+
+
+TimeWire.factory('timeSegments', function($rootScope) {
+    var timewire = document.getElementById('timewire');
+    $rootScope.timewire = new TimeLine(timewire);
+    return $rootScope.timewire;
+});
 
 TimeWire.factory('dataService', function($rootScope, $route, $http, $q) {
     /* this could potentially store data locally in IndexDb */
@@ -58,10 +340,7 @@ TimeWire.factory('dataService', function($rootScope, $route, $http, $q) {
     return get_topic;
 });
 
-/* .when('/cal/', {
-		templateUrl: '/templates/cal.html',
-		controller: 'CalendarCtrl'
-	 }) */
+
 TimeWire.config(function($locationProvider) {
     $locationProvider.html5Mode(true).hashPrefix('!');
 });
--- livewire/livetime/static/js/main.js
+++ livewire/livetime/static/js/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-function in_array(val, array){
+function in_array(val, array) {
     var i = 0;
     for(i=0; i<array.length; i+=1) {
 	if(array[i] === val) {
@@ -9,6 +9,7 @@ function in_array(val, array){
     }
     return false;
 }
+
 Array.prototype.remove = function(from, to) {
   var rest = this.slice((to || from) + 1 || this.length);
   this.length = from < 0 ? this.length + from : from;
@@ -28,6 +29,9 @@ function getOffset( el ) {
     }
     return { top: _y, left: _x };
 }
+
+
+
 TimeWire.controller('Event', function ($scope, $http, $route, $routeParams, $location, dataService) {
     var render_event = function (data) {
 	$scope.events = data[0];
@@ -45,12 +49,20 @@ TimeWire.controller('Event', function ($scope, $http, $route, $routeParams, $loc
     };
 });
 
-TimeWire.controller('TimeLine', function ($scope, $http, $route, $routeParams, $location, $rootScope, dataService) {
-    var init = function(data) {
-	window.addEventListener("resize", init, false);
+TimeWire.controller('TimeLine', function ($scope, $http, $route, $routeParams, $location, $rootScope, dataService, timeSegments) {
+
+
+    var fill_data = function (data) {
+	window.addEventListener("resize", this, false);
 	$scope.new_topic = "";
 	$scope.events = data[0];
 	$scope.articles = data[1];
+
+	if ($scope.events.length) {
+	    $scope.timewire.init(new Date($scope.events[0].fields.start_date), new Date($scope.events[$scope.events.length-1].fields.start_date));
+	}
+
+
 	if ($scope.topics.length > 0) {
 	    if ($scope.events.length>0) {
 		render_timewire();
@@ -59,145 +71,15 @@ TimeWire.controller('TimeLine', function ($scope, $http, $route, $routeParams, $
 	    $location.path('/');
 	}
     };
-    dataService().then(init);
-
-    var get_marker_position = function(date) {
-	var left = get_date_pos(date);
-	var day_txt = "";
-	var month_txt = "";
-	var year_txt = "";
-	var marker_height = 3;
-	if (this.level === 'day') {
-	    day_txt = date.getDate();
-	}
-	if (this.level === 'month' || (this.level=='day' && !this.has_month_marker)) {
-	    month_txt = months_fmt[date.getMonth()];
-	}
-	if (date.getMonth() === 0 || !this.has_year_marker && date.getDate()==1 || (!this.has_year_marker && !this.has_month_marker)) {
-	    year_txt = date.getFullYear();
-	    marker_height = 6;
-	}
-
-	return {'year_txt':year_txt,
-		'month_txt':month_txt,
-		'day_txt':day_txt,
-		'left':left-$scope.date_marker_width/2,
-		'box_left':left - $scope.date_box_width/2,
-		'height':marker_height};
+    var init = function () {
+	dataService().then(fill_data);
     };
-    var get_all_markers = function (level, interval) {
-	var markers = [];
-	if (interval===undefined) {
-	    interval = 1;
-	}
-	var context = {'level':level, 'has_year_marker':has_year_marker(), 'has_month_marker':has_month_marker()};
-	if (level === 'year') {
-	    markers = _.map(
-			  _.map(_.range($scope.start_date.getFullYear()+1, $scope.end_date.getFullYear()+1), function(year) {
-			      return new Date(year, 0, 1);
-			  }),
-			  get_marker_position, context);
-	} else if (level === 'quarter') {
-	    markers = get_all_markers('month', 3);
-	} else if (level === 'month') {
-	    // per-month markers previously to the first "year marker"
-	    var year = $scope.start_date.getFullYear();
-	    markers = _.union(markers, _.map(_.range($scope.start_date.getMonth() + 1 + (12-($scope.start_date.getMonth()+1)) % interval, 12, interval), function (month) {
+    init();
 
-		return new Date(year, month, 1);
-		}));
-
-
-	    // per-month markers between the marked years
-	    _.each(_.range($scope.start_date.getFullYear()+1, $scope.end_date.getFullYear()), function(year) {
-		markers = _.union(markers, _.map(_.range(0, 12, interval), function (month) {
-		    return new Date(year, month, 1);
-		}));
-	    });
-	    // per month markers after the last "year marker"
-	    year = $scope.end_date.getFullYear();
-	    markers = _.union(markers, _.map(_.range(0, $scope.end_date.getMonth()+1, interval), function (month) {
-		    return new Date(year, month, 1);
-		}));
-	    markers = _.map(markers, get_marker_position, context);
-	} else if (level === 'day') {
-	    // per-day markers between the marked months
-	    var date = new Date($scope.start_date);
-	    var i = 0;
-	    while (date < $scope.end_date) {
-		markers[i] = date;
-		date = new Date(date);
-		i += 1;
-		date.setDate(date.getDate() + 1);
-
-		    //date += 1000*60*60*24; // day
-	    }
-	    markers = _.map(markers, get_marker_position, context);
-	}
-	return markers;
-    };
-    var has_month_marker = function () {
-	$scope.start_date.getMonth();
-	if (has_year_marker()) {
-	    return true;
-	} else if (($scope.end_date.getMonth() - $scope.start_date.getMonth()) > 0){
-	    return true;
-	}
-	return false;
-    };
-    var has_year_marker = function () {
-	if (($scope.end_date.getFullYear() - $scope.start_date.getFullYear()) > 0) {
-	    return true;
-	}
-	return false;
-    };
-
-    var get_date_markers = function () {
-	var max_markers = $scope.timeline_width/($scope.date_box_width*2.5);
-	$scope.chosen_level = "year";
-	var levels = _.map(["year", "quarter", "month", "day"], how_many_markers);
-	    _.each(levels, function(level) {
-		if (level[1] <= max_markers) {
-		    $scope.chosen_level = level[0];
-		}
-	    });
-	return get_all_markers($scope.chosen_level);
-    };
-
-    var how_many_markers = function (level) {
-	var num_markers = 0;
-	if (level === "year") {
-	    num_markers =  _.range($scope.start_date.getFullYear(), $scope.end_date.getFullYear()).length;
-	} else if (level === "quarter") {
-	    num_markers = how_many_markers('month')[1]/3;
-	} else if (level === "month") {
-	    var years = how_many_markers('year')[1];
-	    num_markers = Math.max((years-1), 0) * 12;
-	    if (years>0) {
-		num_markers += _.range(0, $scope.end_date.getMonth()+1).length;
-		num_markers += _.range($scope.start_date.getMonth(), 12).length;
-	    } else {
-		num_markers += _.range($scope.start_date.getMonth(), $scope.end_date.getMonth()).length;
-	    }
-	} else if (level === "day") {
-	    var months = how_many_markers("month")[1];
-	    num_markers = Math.max((months-1), 0) * 30; // approximation of 30 days to month
-	    if (months>0) {
-		num_markers += _.range(0, $scope.end_date.getDate()).length;
-		num_markers += _.range($scope.start_date.getDate(), 30).length;
-	    } else {
-		num_markers += _.range($scope.start_date.getDate(), $scope.end_date.getDate()+1).length;
-	    }
-	}
-	return [level, num_markers];
-    };
-    var get_date_pos = function (event_start_date) {
-	return ($scope.timeline_width * (event_start_date - $scope.start_date)/$scope.duration).toFixed(0);
-    };
     var insert_event = function (ev, index, opts) {
 	ev.fields.start_date = new Date(ev.fields.start_date);
 	ev.fields.end_date = new Date(ev.fields.end_date);
-	ev.left = get_date_pos(ev.fields.start_date);
+	ev.left = $scope.timewire.get_date_pos(ev.fields.start_date);
 	ev.box = {};
 	ev.box.width = opts.box_width;
 	ev.box.left = ev.left - ((ev.box.width/2) + opts.box_padding);
@@ -256,26 +138,11 @@ TimeWire.controller('TimeLine', function ($scope, $http, $route, $routeParams, $
 	    });
     };
     var render_timewire = function () {
-	$scope.start_date = new Date($scope.events[0].fields.start_date);
-	$scope.end_date = new Date($scope.events[$scope.events.length-1].fields.start_date);
-
-	$scope.duration = $scope.end_date - $scope.start_date;
-
-	var active_region = document.getElementById('active_region');
-	var timeline_segments =  document.getElementsByClassName('timeline'); //only one for now
-	_.each(timeline_segments, function (seg) {
-	    seg.width = active_region.width + "px";
-	    seg.left = active_region.offsetLeft + "px";
-	});
-	$scope.timeline_width = active_region.offsetWidth;
-	$scope.date_box_width = 35;
-	$scope.date_marker_width = 2;
-	$scope.day_marker_width = 1;
-	$scope.all_markers = get_date_markers();
+	$scope.all_markers = $scope.timewire.get_date_markers();
 	var opts = {
-	    "box_width": $scope.timeline_width/10,
-	    "box_padding":$scope.timeline_width/260,
-	    "box_spacing": $scope.timeline_width/260,
+	    "box_width": $scope.timewire.width/10,
+	    "box_padding":$scope.timewire.width/260,
+	    "box_spacing": $scope.timewire.width/260,
 	    "layer_height": 100}; //layer_height should be set dynamically
 
 	$scope.layers = _.map(_.range(0, 3), function (layer_index) {
@@ -333,12 +200,6 @@ TimeWire.controller('TimeLine', function ($scope, $http, $route, $routeParams, $
     $scope.add = function () {
 	create_event();
     };
-    $scope.zooming=false;
-    var x_pos = undefined;
-    var y_pos = undefined;
-    var ele_width = undefined;
-    var ele_height = undefined;
-    var timeline = document.getElementById("timeline");
 
     $scope.stop_prop = function ($event) {
 	$event.stopPropagation();
@@ -347,21 +208,44 @@ TimeWire.controller('TimeLine', function ($scope, $http, $route, $routeParams, $
 	$event.stopPropagation();
 	$location.path("/topics/" + $rootScope.topics.join(',') + "/event/" + event_id);
     };
+
     $scope.start_scroll = function ($event) {
-	console.log($event);
-	$scope.start_pos = $event.pageX;
+	$scope.timewire.handle_dragstart($event);
 	document.body.addEventListener("pointermove", $scope.scroll_move, false);
 	document.body.addEventListener("pointerup", $scope.scroll_end, false);
     };
+    var scroll = function($event) {
+	$scope.timewire.handle_drag($event);
+	$scope.$apply();
+    };
+    var throttled = _.throttle(scroll, 25);
     $scope.scroll_move = function ($event) {
-	var left = timeline.offsetLeft + ($event.pageX - $scope.start_pos);
-	$scope.start_pos = $event.pageX;
- 	timeline.style.left = left + "px";
+	throttled($event);
     };
     $scope.scroll_end = function ($event) {
+	$scope.timewire.handle_dragend($event);
+	$scope.$apply();
 	document.body.removeEventListener("pointermove", $scope.scroll_move, false);
 	document.body.removeEventListener("pointerup", $scope.scroll_end, false);
     };
+
+
+    $scope.zoom_in = function (factor, zoom_point) {
+	$scope.timewire.zoom(factor, zoom_point);
+	render_timewire();
+    };
+    $scope.zoom_out = function (factor) {
+	$scope.timewire.zoom(1/factor);
+	$scope.all_markers = $scope.timewire.get_date_markers();
+	// implement special behaviour for maximum zoomed out level
+    };
+
+    $scope.zooming=false;
+    var x_pos = undefined;
+    var y_pos = undefined;
+    var ele_width = undefined;
+    var ele_height = undefined;
+
     $scope.start_zoom = function ($event) {
 	x_pos = getOffset($event.target).left;
 	ele_width = $event.target.offsetWidth;
@@ -369,9 +253,7 @@ TimeWire.controller('TimeLine', function ($scope, $http, $route, $routeParams, $
 	y_pos = getOffset($event.target).top;
 	$scope.zooming=true;
     };
-
     $scope.stop_zoom = function ($event) {
-
 	if ($event.pageX <= x_pos || $event.pageX >= x_pos + ele_width || $event.pageY <= y_pos-45 || $event.pageY >= y_pos+ele_height+45) {
 	    $scope.zooming=false;
 	}
@@ -380,5 +262,7 @@ TimeWire.controller('TimeLine', function ($scope, $http, $route, $routeParams, $
 
 TimeWire.controller('Upload', function($scope, $cookies) {
     $scope.csrf = $cookies.csrftoken;
+			console.log($scope.csrf);
     var test = 0;
 });
+
